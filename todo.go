db20faf0c3a989638f3cff62f0b7da142f37339c
chmod-git <callofdragonssfarm@gmail.com>
2024-09-11 11:32:32 +0300

    Database structure modified

--- todo.go
+++ todo.go
@@ -2,25 +2,15 @@ package todo
 
 type TodoList struct {
 	Id          int    `json:"id"`
+	UserId      int    `json:"user_id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
-}
-
-type UserList struct {
-	Id     int
-	UserId int
-	ListId int
 }
 
 type TodoItem struct {
 	Id          int    `json:"id"`
+	ListId      int    `json:"list_id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Done        bool   `json:"done"`
-}
-
-type ListItem struct {
-	Id     int
-	ListId int
-	ItemId int
 }
