ba02b5f7ffebf1cce42b213e08a5686805dc6d4c
chmod-git <callofdragonssfarm@gmail.com>
2024-09-12 14:21:24 +0300

    Dependencies injected in repository, services and handlers layers

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"github.com/chmod-git/todo-app"
 	"github.com/chmod-git/todo-app/pkg/handler"
+	"github.com/chmod-git/todo-app/pkg/repository"
+	"github.com/chmod-git/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	router := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	server := new(todo.Server)
 
-	if err := server.Run("8000", router.InitRoutes()); err != nil {
+	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running the server: %v", err)
 	}
 }
