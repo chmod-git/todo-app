6a312036c706dfc4259819ddcd57c951719fff37
chmod-git <callofdragonssfarm@gmail.com>
2024-09-20 17:39:58 +0300

    JWT authentication added

--- user.go
+++ user.go
@@ -1,7 +1,7 @@
 package todo
 
 type User struct {
-	Id       int    `json:"id"`
+	Id       int    `json:"-" db:"id"`
 	Name     string `json:"name"`
 	Username string `json:"username"`
 	Password string `json:"password"`
