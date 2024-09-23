49ab247ea93729cd6d9c67c60f87adb84fec69fc
chmod-git <callofdragonssfarm@gmail.com>
2024-09-23 15:04:16 +0300

    Authorization and authentication final logic added

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -60,3 +60,24 @@ func (s *AuthService) GenerateToken(username, password string) (string, error) {
 
 	return token.SignedString([]byte(signingKey))
 }
+
+func (s *AuthService) ParseToken(token string) (int, error) {
+	parsedToken, err := jwt.ParseWithClaims(token, &TokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Invalid signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(signingKey), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := parsedToken.Claims.(*TokenClaims)
+	if !ok {
+		return 0, fmt.Errorf("invalid token claims")
+	}
+
+	return claims.UserId, nil
+}
